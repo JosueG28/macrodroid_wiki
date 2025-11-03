dd0c87a07fadd0978e1a6f4d73a5a0b12de5488f
Josué <74258133+JosueG28@users.noreply.github.com>
2025-11-03 14:07:17 -0600

    Deployed 93e25f9 with MkDocs version: 1.6.1

---
+++ assets/bmc.js
@@ -0,0 +1,11 @@
+var script = document.createElement('script');
+script.src = "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js";
+script.setAttribute("data-name", "BMC-Widget");
+script.setAttribute("data-cfasync", "false");
+script.setAttribute("data-id", "josueg28");
+script.setAttribute("data-description", "Support me on Buy me a coffee!");
+script.setAttribute("data-color", "#40DCA5");
+script.setAttribute("data-position", "Right");
+script.setAttribute("data-x_margin", "18");
+script.setAttribute("data-y_margin", "18");
+document.body.appendChild(script);
